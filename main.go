0fe7bc7e0bdb1b743ba8df740768bb80b3d7acd9
Barti <bartosz.majkowski@wp.pl>
2024-04-05 16:41:13 +0200

    implement router to main

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"motorcycles/routes"
+	"motorcycles/service"
 	"os"
 )
 
 func main() {
 	router := gin.Default()
+
+	routes.CarsRoute(router)
+	service.ConnectDB()
 
 	errRouter := router.Run(os.Getenv("MOTORCYCLES_URI"))
 	if errRouter != nil {
